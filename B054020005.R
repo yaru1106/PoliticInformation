9b84381bedc609bb23950c9f140dd281e415408a
yaru1106 <46812689+yaru1106@users.noreply.github.com>
2019-03-11 16:58:05 +0800

    Update B054020005.R

--- B054020005.R
+++ B054020005.R
@@ -16,14 +16,23 @@ min(data07$All_Reaction_Count)
 
 data07$Page_Name[which(data07$All_Reaction_Count==121446)]
 
-#8~12月提到韓國瑜和陳其邁的文章->關心熱度
+#8~12月提到韓國瑜的文章->關心熱度
 length(grep("韓國瑜", data07$Message)) #1272篇
 length(grep("韓國瑜", data08$Message)) #2640篇
 length(grep("韓國瑜", data09$Message)) #3264篇
 length(grep("韓國瑜", data10$Message)) #10937篇
 length(grep("韓國瑜", data11$Message)) #21561篇
 length(grep("韓國瑜", data12$Message)) #14379篇
+store1 <- c(length(grep("韓國瑜", data07$Message)),length(grep("韓國瑜", data08$Message)),length(grep("韓國瑜", data09$Message)),length(grep("韓國瑜", data10$Message)),length(grep("韓國瑜", data11$Message)),length(grep("韓國瑜", data12$Message)))
 
+library(ggplot2)
+BaybyGowth <- data.frame( 
+  month = c(7, 8, 9, 10, 11,12), 
+  store1) 
+print(BaybyGowth) 
+ggplot(BaybyGowth, aes(x = month, y = store1)) + geom_bar(stat = "identity") 
+
+#8~12月提到韓冰的文章->關心熱度
 length(grep("韓冰", data07$Message)) #0篇
 length(grep("韓冰", data08$Message)) #5篇
 length(grep("韓冰", data09$Message)) #5篇
@@ -31,13 +40,29 @@ length(grep("韓冰", data10$Message)) #7篇
 length(grep("韓冰", data11$Message)) #529篇
 length(grep("韓冰", data12$Message)) #1158篇
 
+store2 <- c(length(grep("韓冰", data07$Message)),length(grep("韓冰", data08$Message)),length(grep("韓冰", data09$Message)),length(grep("韓冰", data10$Message)),length(grep("韓冰", data11$Message)),length(grep("韓冰", data12$Message)))
+BaybyGowth2 <- data.frame( 
+  month = c(7, 8, 9, 10, 11,12), 
+  store2) 
+print(BaybyGowth2) 
+ggplot(BaybyGowth2, aes(x = month, y = store2)) + geom_bar(stat = "identity") 
+
+#8~12月提到陳其邁的文章->關心熱度
+store3 <- c(length(grep("陳其邁", data07$Message)),length(grep("陳其邁", data08$Message)),length(grep("陳其邁", data09$Message)),length(grep("陳其邁", data10$Message)),length(grep("陳其邁", data11$Message)),length(grep("陳其邁", data12$Message)))
+BaybyGowth3 <- data.frame( 
+  month = c(7, 8, 9, 10, 11,12), 
+  store3) 
+print(BaybyGowth3) 
+ggplot(BaybyGowth3, aes(x = month, y = store3)) + geom_bar(stat = "identity") 
+
+
+
 length(grep("陳其邁", data07$Message)) #509篇
 length(grep("陳其邁", data08$Message)) #840篇
 length(grep("陳其邁", data09$Message)) #1263篇
 length(grep("陳其邁", data10$Message)) #3241篇
 length(grep("陳其邁", data11$Message)) #7887篇
 length(grep("陳其邁", data12$Message)) #1960篇
-
 
 #8~12月提到韓國瑜和陳其邁發文數->社群經營
 length(grep("韓國瑜", data07$Page_Name)) #105篇
